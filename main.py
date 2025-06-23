dce4df93759e7e6d59942d84609e139715ba6a74
RobertsBots <iiunii001@gmail.com>
2025-06-23 20:56:19 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,43 +1,41 @@
+
 import os
-from telegram.ext import ApplicationBuilder
+from fastapi import FastAPI, Request
+from telegram import Update
+from telegram.ext import Application, CallbackQueryHandler
+from telegram.ext import CommandHandler
+from telegram.ext import defaults
+
 from core.start import start_handler
 from core.add import add_handler
 from core.remove import remove_handler
 from core.list_wallets import list_handler
 from core.profit import profit_handler
-from core.handlers import callback_handler
-from dotenv import load_dotenv
-from fastapi import FastAPI
-import asyncio
 
-load_dotenv()
-BOT_TOKEN = os.getenv("BOT_TOKEN")
+TOKEN = os.getenv("BOT_TOKEN")
 WEBHOOK_URL = os.getenv("WEBHOOK_URL")
 
-application = ApplicationBuilder().token(BOT_TOKEN).build()
+app = FastAPI()
+defaults = defaults.Defaults(parse_mode="HTML")
+
+application = Application.builder().token(TOKEN).defaults(defaults).build()
+
 application.add_handler(start_handler)
 application.add_handler(add_handler)
 application.add_handler(remove_handler)
 application.add_handler(list_handler)
 application.add_handler(profit_handler)
-application.add_handler(callback_handler)
 
-# FastAPI App für Webhook
-app = FastAPI()
-
-@app.post(f"/{BOT_TOKEN}")
-async def webhook(update: dict):
-    await application.update_queue.put(update)
+@app.post("/webhook")
+async def webhook(request: Request):
+    data = await request.json()
+    update = Update.de_json(data, application.bot)
+    await application.process_update(update)
     return {"status": "ok"}
 
-async def run():
-    await application.initialize()
-    await application.start()
-    await application.bot.set_webhook(url=WEBHOOK_URL)
-    await application.updater.start_webhook(
+if __name__ == "__main__":
+    application.run_webhook(
         listen="0.0.0.0",
-        port=int(os.environ.get("PORT", 8000)),
+        port=int(os.getenv("PORT", "8000")),
         webhook_url=WEBHOOK_URL
     )
-
-asyncio.run(run())
