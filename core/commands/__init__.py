1d653c93dd8e447b451fe2788288c650a853e4ab
RobertsBots <iiunii001@gmail.com>
2025-06-24 22:22:12 +0200

    Update __init__.py

--- core/commands/__init__.py
+++ core/commands/__init__.py
@@ -2,7 +2,7 @@ from .add_cmd import router as add_wallet_cmd
 from .rm_cmd import remove_wallet_cmd, handle_rm_callback
 from .list_cmd import list_wallets_cmd
 from .profit_cmd import router as profit_cmd_router, handle_profit_callback
-from .start_cmd import start_cmd
+from .start_cmd import router as start_cmd
 from .finder_cmd import finder_menu_cmd, handle_finder_selection  # falls vorhanden
 
 __all__ = [
