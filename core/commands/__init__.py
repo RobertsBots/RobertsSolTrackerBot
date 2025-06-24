bd3ffac835832c740c8279adce15a627362d1b3a
RobertsBots <iiunii001@gmail.com>
2025-06-24 20:44:11 +0200

    Update __init__.py

--- core/commands/__init__.py
+++ core/commands/__init__.py
@@ -1,1 +1,13 @@
+from .start_cmd import start_cmd
+from .add_wallet_cmd import add_wallet_cmd
+from .rm_wallet_cmd import rm_wallet_cmd
+from .list_cmd import list_cmd
+from .profit_cmd import profit_cmd
 
+__all__ = [
+    "start_cmd",
+    "add_wallet_cmd",
+    "rm_wallet_cmd",
+    "list_cmd",
+    "profit_cmd"
+]
