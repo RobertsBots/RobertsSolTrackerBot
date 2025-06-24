b8d0210abd5c8f038762e3d01ac56d375eb5b214
RobertsBots <iiunii001@gmail.com>
2025-06-24 18:44:09 +0200

    Update __init__.py

--- core/smartfinder/__init__.py
+++ core/smartfinder/__init__.py
@@ -1,1 +1,8 @@
+# core/smartfinder/__init__.py
 
+"""
+Initialisierungsmodul für den Smart Wallet Finder.
+"""
+
+from .finder import start_smartfinder_job, stop_smartfinder_job
+from .menu import smartfinder_menu_handler, toggle_finder_command
