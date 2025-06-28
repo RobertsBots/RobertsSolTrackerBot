cbd89ad3714104bf59787a8b830221a43fce104d
RobertsBots <iiunii001@gmail.com>
2025-06-28 16:22:32 +0200

    Update cron.py

--- core/cron.py
+++ core/cron.py
@@ -1,3 +1,5 @@
+# core/cron.py
+
 import asyncio
 import logging
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
@@ -5,14 +7,12 @@ from core.run_smart_finder import run_smart_wallet_finder
 
 # Logger
 logger = logging.getLogger(__name__)
-
-# Scheduler Setup
 scheduler = AsyncIOScheduler()
 
-def start_cronjob():
+def setup_cron_jobs(bot):
     logger.info("🚀 Cronjob gestartet: SmartFinder wird alle 30 Minuten ausgeführt.")
     scheduler.add_job(
-        lambda: asyncio.create_task(run_smart_wallet_finder()),
+        lambda: asyncio.create_task(run_smart_wallet_finder(bot)),
         trigger='interval',
         minutes=30,
         id='smart_finder_cron'
