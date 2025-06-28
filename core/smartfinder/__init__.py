864016f9a391dc9d54dbe6fb354a4c5f9952f928
RobertsBots <iiunii001@gmail.com>
2025-06-28 22:51:20 +0200

    Delete core/smartfinder directory

--- core/smartfinder/__init__.py
+++
@@ -1,8 +0,0 @@
-# core/smartfinder/__init__.py
-
-"""
-Initialisierungsmodul für den Smart Wallet Finder.
-"""
-
-from .finder import start_smartfinder_job, stop_smartfinder_job
-from .menu import smartfinder_menu_handler, toggle_finder_command
