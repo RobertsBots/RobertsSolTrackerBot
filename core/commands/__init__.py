355104bc82d63bb5202b0435f61edace79eaac9e
RobertsBots <iiunii001@gmail.com>
2025-06-28 23:35:56 +0200

    Update __init__.py

--- core/commands/__init__.py
+++ core/commands/__init__.py
@@ -2,20 +2,20 @@
 
 from aiogram import Router
 
-# ✅ Einzelrouter importieren
-from .start_cmd import router as start_cmd
-from .add_cmd import router as add_wallet_cmd
+# Subrouter-Importe (alle NUR als Router-Instanzen importieren!)
+from .start_cmd import router as start_cmd_router
+from .add_cmd import router as add_cmd_router
 from .rm_cmd import router as rm_cmd_router
 from .list_cmd import router as list_cmd_router
 from .profit_cmd import router as profit_cmd_router
 from .finder_cmd import router as finder_cmd_router
 
-# ✅ Zentraler Router
+# Haupt-Router definieren
 main_router = Router()
 
-# 🔗 Subrouter exakt einmal einbinden
-main_router.include_router(start_cmd)
-main_router.include_router(add_wallet_cmd)
+# Subrouter einbinden – KEIN dp.include_router() HIER!
+main_router.include_router(start_cmd_router)
+main_router.include_router(add_cmd_router)
 main_router.include_router(rm_cmd_router)
 main_router.include_router(list_cmd_router)
 main_router.include_router(profit_cmd_router)
