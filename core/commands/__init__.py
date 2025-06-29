0447f049fe1097eed6c1a08d86d8b2b59354dcbd
RobertsBots <iiunii001@gmail.com>
2025-06-29 14:23:07 +0200

    Update __init__.py

--- core/commands/__init__.py
+++ core/commands/__init__.py
@@ -1,24 +1,20 @@
 # core/commands/__init__.py
 
-from aiogram import Router
+from aiogram import Dispatcher
 
-# Subrouter-Importe (alle NUR als Router-Instanzen importieren!)
-from .start_cmd import router as start_cmd_router
-from .add_cmd import router as add_cmd_router
-from .rm_cmd import router as rm_cmd_router
-from .list_cmd import router as list_cmd_router
-from .profit_cmd import router as profit_cmd_router
-from .finder_cmd import router as finder_cmd_router
+# Importiere alle Handler-Registrierungsfunktionen
+from .start_cmd import register_handlers as register_start_cmd
+from .add_cmd import register_handlers as register_add_cmd
+from .rm_cmd import register_handlers as register_rm_cmd
+from .list_cmd import register_handlers as register_list_cmd
+from .profit_cmd import register_handlers as register_profit_cmd
+from .finder_cmd import register_handlers as register_finder_cmd
 
-# Haupt-Router definieren
-main_router = Router()
-
-# Subrouter einbinden – KEIN dp.include_router() HIER!
-main_router.include_router(start_cmd_router)
-main_router.include_router(add_cmd_router)
-main_router.include_router(rm_cmd_router)
-main_router.include_router(list_cmd_router)
-main_router.include_router(profit_cmd_router)
-main_router.include_router(finder_cmd_router)
-
-__all__ = ["main_router"]
+# Hauptfunktion zur Handler-Registrierung
+def main_router(dp: Dispatcher):
+    register_start_cmd(dp)
+    register_add_cmd(dp)
+    register_rm_cmd(dp)
+    register_list_cmd(dp)
+    register_profit_cmd(dp)
+    register_finder_cmd(dp)
