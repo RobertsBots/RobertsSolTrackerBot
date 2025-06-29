1c5d3d1e2fb866740baa2e9fe863be0c1b99d371
RobertsBots <iiunii001@gmail.com>
2025-06-30 00:19:58 +0200

    Update __init__.py

--- core/commands/__init__.py
+++ core/commands/__init__.py
@@ -1,12 +1,12 @@
 from aiogram import Dispatcher
 
 # ✅ Handler für Telegram-Kommandos
-from .start_cmd import register_handlers as register_start_cmd
-from .add_cmd import register_handlers as register_add_cmd
-from .rm_cmd import register_handlers as register_rm_cmd
-from .list_cmd import register_handlers as register_list_cmd
-from .profit_cmd import register_handlers as register_profit_cmd
-from .finder_cmd import register_handlers as register_finder_cmd
+from .start_cmd import register_start_cmd
+from .add_cmd import register_add_cmd
+from .rm_cmd import register_rm_cmd
+from .list_cmd import register_list_cmd
+from .profit_cmd import register_profit_cmd
+from .finder_cmd import register_finder_cmd
 
 # ✅ Button-Callback-Handler
 from .callbacks import register_callback_buttons
